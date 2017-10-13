acf14d279feea736938531b097931fb597152024
jtgerson <32581153+jtgerson@users.noreply.github.com>
2017-10-13 08:09:23 -0400

    Daily

--- Python_code/Manipulate_HP_data.py
+++ Python_code/Manipulate_HP_data.py
@@ -1,32 +1,66 @@
 # PANDAS
 import pandas as pd
+# numpy
 import numpy as np
 
-df = pd.read_csv('/Users/jggerson/Desktop/HP1.csv') #import data from location
+#installing modules
+#python -m pip install fuzzywuzzy
+
+
+
+#import data from location
+df = pd.read_csv('/Users/jggerson/Desktop/HP1.csv') 
 df1 = pd.read_csv('/Users/jggerson/Desktop/HP_Listen.csv')
+#cleanse date by dropping October, and Fanstasic Beasts
+df = df.drop(df[df.yr_mth_name == "10/1/2017"].index)
+#df = df.drop(df[df.product_royalty_earner == "Pottermore Limited - Fantastic Beasts"].index)
+#df = df.drop(df[df.product_royalty_earner == "Pottermore Limited / German Translations / Phantastische Tierwesen"].index)
 
-#print(df.head(20)) #print first 20 lines of each field
-#print(df.parent_product_id.unique()) #print unique values in parent_product_id field
+#print first 20 lines of each field
+#print(df.head(20)) 
 
+#print unique values in parent_product_id field
+#print(df.parent_product_id.unique()) 
 
-#print(df.shape) #counts rows and columns
+#counts rows and columns
+#print(df.shape) 
 
-#print(df.units.sum()) #sum units
-
+#sum units
+print(df.units.sum()) 
 
 #Extract units by sale type
-#print(df.groupby(by=['sale_type'])['units'].sum()) #group units by sale type
+#print(df.groupby(by=['sale_type'])['units'].sum()) 
 
+#Extract units by royalty earner
+print(df.groupby(by=['product_royalty_earner'])['units'].sum()) 
 
-print(df.pivot_table("units",index='sale_type',columns=['marketplace_name'],aggfunc='sum')) #sum units by sale type and marketplace
+#sum units by sale type and marketplace
+#print(df.pivot_table("units",index='sale_type',columns=['marketplace_name'],aggfunc='sum')) 
 
+#sum units by royalty earner and marketplace
+#print(df.pivot_table("units",index='product_royalty_earner',columns=['marketplace_name'],aggfunc='sum')) 
 
-#print(df.loc[df['sale_type'] =="AL", 'units'].sum()) #sum AL Units
+#sum AL Units
+#print(df.loc[df['sale_type'] =="AL", 'units'].sum())
 
-#print(df.groupby('sale_type')['units'].sum()["AL"]) #sum AL Units (alternatively)
+#sum AL Units (alternatively)
+#print(df.groupby('sale_type')['units'].sum()["AL"]) 
 
-#print((df1.seconds.sum())/60) #total minutes consumed
+#total minutes consumed
+#print((df1.seconds.sum())/60) 
 
-#df1.min=(df1.seconds.sum())/60 #df1.min converst my seconds to minutes
+#converts my seconds to minutes
+#df1.min=(df1.seconds.sum())/60 #df1.min 
 
-print(df1.groupby(by=['product_royalty_earner'])['seconds'].sum()) minutes by earner
+#minutes by earner
+#print(df1.groupby(by=['product_royalty_earner'])['seconds'].sum()) 
+
+#sum units other than AL
+#print(df.loc[df['sale_type'] !="AL", 'units'].sum()) 
+
+#units not AL and not ALC sale type
+#print(df.loc[(df['sale_type'] !="AL") & (df['sale_type'] !="ALC"), 'units'].sum())  
+
+#sum units that aren't in 10/1/2017 and not fantastic beasts
+#print(df.loc[(df['yr_mth_name'] !="10/1/2017") & (df['product_royalty_earner'] !="Pottermore Limited - Fantastic Beasts") & (df['product_royalty_earner'] !="Pottermore Limited / German Translations / Phantastische Tierwesen"), 'units'].sum())
+
---
+++ Python_code/Python_sample_code.py
@@ -0,0 +1,42 @@
+# Print Welcome Message
+#print ('helo world')
+
+#python -m pip install fuzzywuzzy
+
+from fuzzywuzzy import fuzz 
+from fuzzywuzzy import process
+
+from difflib import SequenceMatcher as SM 
+
+
+print(fuzz.ratio("Acme Factory", "Acme Factory Inc."))
+
+#by using the partial ratio, it can determine that the two are really identical
+print(fuzz.partial_ratio("Acme Factory", "Acme Factory Inc."))
+
+#100% Score
+print(fuzz.ratio("Barack Obama", "Barack Obama")) #100% Score
+
+print(fuzz.ratio("Barack Obama", "Barack H. Obama")) #89% score
+
+print(fuzz.ratio("Barack H Obama", "Barack H. Obama")) #97% score
+
+#partial ratio does work as well as ratio because the extra token is in the middle
+print(fuzz.partial_ratio("Barack H Obama", "Barack H. Obama")) #93% score
+
+#The token_* functions split the string on white-spaces, lowercase everything and get rid of non-alpha non-numeric characters, which means punctuation is ignored (as well as weird unicode symbols)
+print(fuzz.token_sort_ratio('Barack Obama', 'Barack H. Obama')) #92% score extra letter in middle lowers score with sore
+
+print(fuzz.token_set_ratio('Barack Obama', 'Barack H. Obama')) # 100 token_set recognizes 
+
+print(fuzz.token_sort_ratio('Barack H Obama', 'Barack H. Obama')) # 100
+
+print(fuzz.token_set_ratio('Barack H Obama', 'Barack H. Obama')) # 100 
+
+query = 'Barack Obama'
+choices = ['Barack H Obama', 'Barack H. Obama' , 'B. Obama']
+# Get a list of matches ordered by score, default limit to 5
+print(process.extract(query, choices))
+
+# If we want only the top one
+print(process.extractOne(query, choices))
